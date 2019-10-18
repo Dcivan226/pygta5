9131bb44d5fb8a50250948512a135c6d87e5340f
Dcivan226 <52780161+Dcivan226@users.noreply.github.com>
2019-10-18 15:55:09 +0800

    修改测试

--- Tutorial Codes/Part 1-7/part-2-and-3-keyboard-input-and-basic-OpenCV.py
+++ Tutorial Codes/Part 1-7/part-2-and-3-keyboard-input-and-basic-OpenCV.py
@@ -15,19 +15,25 @@ def process_img(image):
 
 def main():
     
-    for i in list(range(4))[::-1]:
-        print(i+1)
-        time.sleep(1)
+    # for i in list(range(4))[::-1]:
+    #     print(i+1)
+    #     time.sleep(1)
 
     last_time = time.time()
     while True:
-        PressKey(W)
+        #按键W
+        # PressKey(W)
         screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
         #print('Frame took {} seconds'.format(time.time()-last_time))
         last_time = time.time()
         new_screen = process_img(screen)
         cv2.imshow('window', new_screen)
-        #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
+
+
+#开始执行函数
+if __name__ == '__main__':
+    main()
